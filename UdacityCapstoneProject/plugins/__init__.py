5dcc50406d2d5e291d7e3b56218db4c6f8fc40e5
Anzhe Meng <1310750076@qq.com>
2021-05-31 21:00:05 -0400

    Add files via upload

---
+++ UdacityCapstoneProject/plugins/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import division, absolute_import, print_function
+from airflow.plugins_manager import AirflowPlugin
+from plugins import operators
+
+
+# Defining the plugin class
+class UdacityPlugin(AirflowPlugin):
+    name = "udacity_plugin"
+    operators = [
+        operators.loadTableOperator
+    ]
---
+++ UdacityCapstoneProject/plugins/operators/__init__.py
@@ -0,0 +1,7 @@
+from plugins.operators.load_table import loadTableOperator
+
+__all__ = [
+    'loadTableOperator'
+]
+
+
